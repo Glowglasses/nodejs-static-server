86dd37e6ea35c0c92403d1d017c258d7f9017cbc
Glowglasses <42904031+Glowglasses@users.noreply.github.com>
2021-06-16 16:18:46 +0800

    Update static_server.js

--- static_server.js
+++ static_server.js
@@ -1,53 +1,60 @@
-var http = require('http')
-var fs = require('fs')
-var url = require('url')
-var port = process.argv[2]
+let http = require('http')
+let fs = require('fs')
+let url = require('url')
+let port = process.argv[2]
 
 if(!port){
-  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
-  process.exit(1)
+    console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+    process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
-  var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
-  var queryString = ''
-  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
-  var path = parsedUrl.pathname
-  var query = parsedUrl.query
-  var method = request.method
-
-  /******** 从这里开始看，上面不要看 ************/
+let server = http.createServer(function(request, response){
+    let parsedUrl = url.parse(request.url, true)
+    let pathWithQuery = request.url
+    let queryString = ''
+    if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+    let path = parsedUrl.pathname
+    let query = parsedUrl.query
+    let method = request.method
+    
+    /******** 从这里开始看，上面不要看 ************/
 
 //   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
-  mimeHashList = {"text":"text/plain","html":"text/html","jpg":"image/jpeg","json":"text/json","js":"text/javascript"}
-
-  if(path === '/'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(fs.readFileSync("public/index.html"))
-    response.end()
-  }else {
-    let file
-    pathname = path.substr(1)
-    try{
-        file = fs.readFileSync('public/' + pathname)
-    }catch{
-        response.statusCode = 404
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`你输入的路径不存在对应的内容`)
-        response.end()
-    }
-    if(file !== undefined){
-        response.statusCode = 200
-        response.setHeader('Content-Type', `${mimeHashList[path.substr(path.lastIndexOf(".") + 1).toLowerCase()]};charset=utf-8`)
-        response.write(file)
-        response.end()
+    let mimeHashList = {
+        "text": "text/plain",
+        "html": "text/html",
+        "jpg": "image/jpeg",
+        "json": "text/json",
+        "js": "text/javascript"
     }
     
-  }
-
-  /******** 代码结束，下面不要看 ************/
+    let pathname;
+    if (path === '/') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.write(fs.readFileSync("public/index.html"))
+        response.end()
+    } else {
+        let file
+        pathname = path.substr(1)
+        try {
+            file = fs.readFileSync('public/' + pathname)
+        } catch {
+            response.statusCode = 404
+            response.setHeader('Content-Type', 'text/html;charset=utf-8')
+            response.write(`你输入的路径不存在对应的内容`)
+            response.end()
+        }
+        if (file !== undefined) {
+            response.statusCode = 200
+            response.setHeader('Content-Type', `${mimeHashList[path.substr(path.lastIndexOf(".") + 1).toLowerCase()]};charset=utf-8`)
+            response.write(file)
+            response.end()
+        }
+        
+    }
+    
+    /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
